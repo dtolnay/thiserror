5ebbfb80e15c282c024e0f6d48a06aae33346427
David Tolnay <dtolnay@gmail.com>
2020-03-22 22:57:56 -0700

    Add expr test cases from rustup

--- tests/test_expr.rs
+++ tests/test_expr.rs
@@ -4,7 +4,7 @@ use thiserror::Error;
 // Some of the elaborate cases from the rcc codebase, which is a C compiler in
 // Rust. https://github.com/jyn514/rcc/blob/0.8.0/src/data/error.rs
 #[derive(Error, Debug)]
-pub enum Error {
+pub enum CompilerError {
     #[error("cannot shift {} by {maximum} or more bits (got {current})", if *.is_left { "left" } else { "right" })]
     TooManyShiftBits {
         is_left: bool,
@@ -32,6 +32,23 @@ pub enum Error {
     IntegerOverflow2 { is_signed: Option<bool> },
 }
 
+// Examples drawn from Rustup.
+#[derive(Error, Debug)]
+pub enum RustupError {
+    #[error(
+        "toolchain '{name}' does not contain component {component}{}",
+        .suggestion
+            .as_ref()
+            .map(|s| format!("; did you mean '{}'?", s))
+            .unwrap_or_else(String::new),
+    )]
+    UnknownComponent {
+        name: String,
+        component: String,
+        suggestion: Option<String>,
+    },
+}
+
 fn assert<T: Display>(expected: &str, value: T) {
     assert_eq!(expected, value.to_string());
 }
@@ -40,19 +57,31 @@ fn assert<T: Display>(expected: &str, value: T) {
 fn test_rcc() {
     assert(
         "cannot shift left by 32 or more bits (got 50)",
-        Error::TooManyShiftBits {
+        CompilerError::TooManyShiftBits {
             is_left: true,
             maximum: 32,
             current: 50,
         },
     );
 
-    assert("#error A B C", Error::User(vec!["A", "B", "C"]));
+    assert("#error A B C", CompilerError::User(vec!["A", "B", "C"]));
 
     assert(
         "overflow while parsing signed integer literal",
-        Error::IntegerOverflow {
+        CompilerError::IntegerOverflow {
             is_signed: Some(true),
+        },
+    );
+}
+
+#[test]
+fn test_rustup() {
+    assert(
+        "toolchain 'nightly' does not contain component clipy; did you mean 'clippy'?",
+        RustupError::UnknownComponent {
+            name: "nightly".to_owned(),
+            component: "clipy".to_owned(),
+            suggestion: Some("clippy".to_owned()),
         },
     );
 }
