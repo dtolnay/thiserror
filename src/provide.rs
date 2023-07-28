1899bdd664b9228af1cf7d7ffc47ff5d6ca9deee
wayne warren <wayne.warren.s@gmail.com>
2023-07-28 17:43:36 -0600

    update test_bactrace to use dyn Error for generic member access

--- src/provide.rs
+++ src/provide.rs
@@ -1,15 +1,15 @@
-use std::any::{Demand, Provider};
+use std::any::Request;
 
 pub trait ThiserrorProvide: Sealed {
-    fn thiserror_provide<'a>(&'a self, demand: &mut Demand<'a>);
+    fn thiserror_provide<'a>(&'a self, request: &mut Request<'a>);
 }
 
-impl<T: Provider + ?Sized> ThiserrorProvide for T {
+impl<T: std::error::Error + ?Sized> ThiserrorProvide for T {
     #[inline]
-    fn thiserror_provide<'a>(&'a self, demand: &mut Demand<'a>) {
-        self.provide(demand);
+    fn thiserror_provide<'a>(&'a self, request: &mut Request<'a>) {
+        self.provide(request);
     }
 }
 
 pub trait Sealed {}
-impl<T: Provider + ?Sized> Sealed for T {}
+impl<T: ?Sized> Sealed for T {}
