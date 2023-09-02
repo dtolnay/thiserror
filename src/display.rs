bd640d10e638d82458f9e79b80e68147eeb12bf9
David Tolnay <dtolnay@gmail.com>
2023-09-02 12:51:52 -0700

    Remove unsafe code from AsDisplay

--- src/display.rs
+++ src/display.rs
@@ -2,52 +2,45 @@ use std::fmt::{self, Display};
 use std::path::{Path, PathBuf};
 
 #[doc(hidden)]
-pub trait AsDisplay {
-    type Target: Display + ?Sized;
+pub trait AsDisplay<'a> {
+    type Target: Display;
 
-    fn as_display(&self) -> &Self::Target;
+    fn as_display(&'a self) -> Self::Target;
 }
 
-impl<T: Display> AsDisplay for &T {
-    type Target = T;
+impl<'a, T> AsDisplay<'a> for &T
+where
+    T: Display + 'a,
+{
+    type Target = &'a T;
 
-    fn as_display(&self) -> &Self::Target {
-        self
+    fn as_display(&'a self) -> Self::Target {
+        *self
     }
 }
 
-impl AsDisplay for Path {
-    type Target = PathDisplay;
+impl<'a> AsDisplay<'a> for Path {
+    type Target = PathDisplay<'a>;
 
     #[inline]
-    fn as_display(&self) -> &Self::Target {
-        PathDisplay::new(self)
+    fn as_display(&'a self) -> Self::Target {
+        PathDisplay(self)
     }
 }
 
-impl AsDisplay for PathBuf {
-    type Target = PathDisplay;
+impl<'a> AsDisplay<'a> for PathBuf {
+    type Target = PathDisplay<'a>;
 
     #[inline]
-    fn as_display(&self) -> &Self::Target {
-        PathDisplay::new(self.as_path())
+    fn as_display(&'a self) -> Self::Target {
+        PathDisplay(self.as_path())
     }
 }
 
 #[doc(hidden)]
-#[repr(transparent)]
-pub struct PathDisplay(Path);
+pub struct PathDisplay<'a>(&'a Path);
 
-impl PathDisplay {
-    #[inline]
-    fn new(path: &Path) -> &Self {
-        // SAFETY: PathDisplay is repr(transparent) so casting pointers between
-        // it and its payload is safe.
-        unsafe { &*(path as *const Path as *const Self) }
-    }
-}
-
-impl Display for PathDisplay {
+impl<'a> Display for PathDisplay<'a> {
     #[inline]
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
         self.0.display().fmt(formatter)
