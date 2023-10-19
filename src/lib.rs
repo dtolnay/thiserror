a7d220d7915fb888413aa7978efd70f7006bda9d
David Tolnay <dtolnay@gmail.com>
2023-10-19 11:10:16 -0700

    Release 1.0.50

--- src/lib.rs
+++ src/lib.rs
@@ -228,7 +228,7 @@
 //!
 //!   [`anyhow`]: https://github.com/dtolnay/anyhow
 
-#![doc(html_root_url = "https://docs.rs/thiserror/1.0.49")]
+#![doc(html_root_url = "https://docs.rs/thiserror/1.0.50")]
 #![allow(
     clippy::module_name_repetitions,
     clippy::needless_lifetimes,
