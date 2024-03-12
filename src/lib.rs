df8bffad18e408e9b88d3360797506fc1282babe
David Tolnay <dtolnay@gmail.com>
2024-03-11 19:55:46 -0700

    Release 1.0.58

--- src/lib.rs
+++ src/lib.rs
@@ -228,7 +228,7 @@
 //!
 //!   [`anyhow`]: https://github.com/dtolnay/anyhow
 
-#![doc(html_root_url = "https://docs.rs/thiserror/1.0.57")]
+#![doc(html_root_url = "https://docs.rs/thiserror/1.0.58")]
 #![allow(
     clippy::module_name_repetitions,
     clippy::needless_lifetimes,
