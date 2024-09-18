2614b5353c1f9026b1ae999c9439fa14a8b2340f
Robert Jördens <rj@quartiq.de>
2024-09-18 15:38:08 +0200

    test_no_std: reduce

--- tests/test_no_std.rs
+++ tests/test_no_std.rs
@@ -1,56 +1,23 @@
-// There is no negative required-features to suppress this test
-// for not(feature = "std").
-#![cfg(not(feature = "std"))]
 #![no_std]
-
-use core::error::Error as _;
 use thiserror::Error;
 
-#[derive(Error, Debug, Default)]
+#[derive(Error, Debug)]
 #[error("io")]
-pub struct IoError {}
+pub struct IoError;
 
 #[derive(Error, Debug)]
-#[error("implicit source")]
-pub struct ImplicitSource {
-    source: IoError,
-}
-
-#[derive(Error, Debug)]
-#[error("explicit source")]
-pub struct ExplicitSource {
-    source: i32,
-    #[source]
-    io: IoError,
+pub enum MyError {
+    #[error("A")]
+    A,
+    #[error("B {0}")]
+    B(#[from] IoError),
 }
 
 #[test]
-fn test_implicit_source() {
-    let io = IoError::default();
-    let error = ImplicitSource { source: io };
+#[cfg_attr(feature = "std", ignore)]
+fn test_no_std() {
+    use core::error::Error as _;
+
+    let error = MyError::from(IoError);
     error.source().unwrap().downcast_ref::<IoError>().unwrap();
-}
-
-#[test]
-fn test_explicit_source() {
-    let io = IoError::default();
-    let error = ExplicitSource { source: 0, io };
-    error.source().unwrap().downcast_ref::<IoError>().unwrap();
-}
-
-macro_rules! error_from_macro {
-    ($($variants:tt)*) => {
-        #[derive(Error)]
-        #[derive(Debug)]
-        pub enum MacroSource {
-            $($variants)*
-        }
-    }
-}
-
-// Test that we generate impls with the proper hygiene
-#[rustfmt::skip]
-error_from_macro! {
-    #[error("Something")]
-    Variant(#[from] IoError)
 }
