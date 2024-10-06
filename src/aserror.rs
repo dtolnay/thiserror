a72ea77c457bd4e150e8de93b33d8258f1908feb
David Tolnay <dtolnay@gmail.com>
2024-10-07 00:15:12 +0200

    Resolve extra_unused_lifetimes clippy lint
        warning: this lifetime isn't used in the impl
          --> src/aserror.rs:46:6
           |
        46 | impl<'a, T: Error + 'a> Sealed for T {}
           |      ^^
           |
           = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#extra_unused_lifetimes
           = note: `-W clippy::extra-unused-lifetimes` implied by `-W clippy::all`
           = help: to override `-W clippy::all` add `#[allow(clippy::extra_unused_lifetimes)]`

--- src/aserror.rs
+++ src/aserror.rs
@@ -43,7 +43,7 @@ impl<'a> AsDynError<'a> for dyn Error + Send + Sync + UnwindSafe + 'a {
 
 #[doc(hidden)]
 pub trait Sealed {}
-impl<'a, T: Error + 'a> Sealed for T {}
+impl<T: Error> Sealed for T {}
 impl<'a> Sealed for dyn Error + 'a {}
 impl<'a> Sealed for dyn Error + Send + 'a {}
 impl<'a> Sealed for dyn Error + Send + Sync + 'a {}
