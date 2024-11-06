d8ed5fbc2f7476430ff8e50b37f466638cd732f3
David Tolnay <dtolnay@gmail.com>
2024-11-05 19:43:34 -0500

    Allow disabling std dependency on 1.81+

--- src/display.rs
+++ src/display.rs
@@ -1,4 +1,5 @@
 use core::fmt::Display;
+#[cfg(feature = "std")]
 use std::path::{self, Path, PathBuf};
 
 #[doc(hidden)]
@@ -21,6 +22,7 @@ where
     }
 }
 
+#[cfg(feature = "std")]
 impl<'a> AsDisplay<'a> for Path {
     type Target = path::Display<'a>;
 
@@ -30,6 +32,7 @@ impl<'a> AsDisplay<'a> for Path {
     }
 }
 
+#[cfg(feature = "std")]
 impl<'a> AsDisplay<'a> for PathBuf {
     type Target = path::Display<'a>;
 
@@ -42,5 +45,37 @@ impl<'a> AsDisplay<'a> for PathBuf {
 #[doc(hidden)]
 pub trait Sealed {}
 impl<T: Display> Sealed for &T {}
+#[cfg(feature = "std")]
 impl Sealed for Path {}
+#[cfg(feature = "std")]
 impl Sealed for PathBuf {}
+
+// Add a synthetic second impl of AsDisplay to prevent the "single applicable
+// impl" rule from making too weird inference decision based on the single impl
+// for &T, which could lead to code that compiles with thiserror's std feature
+// off but breaks under feature unification when std is turned on by an
+// unrelated crate.
+#[cfg(not(feature = "std"))]
+mod placeholder {
+    use super::{AsDisplay, Sealed};
+    use core::fmt::{self, Display};
+
+    pub struct Placeholder;
+
+    impl<'a> AsDisplay<'a> for Placeholder {
+        type Target = Self;
+
+        #[inline]
+        fn as_display(&'a self) -> Self::Target {
+            Placeholder
+        }
+    }
+
+    impl Display for Placeholder {
+        fn fmt(&self, _formatter: &mut fmt::Formatter) -> fmt::Result {
+            unreachable!()
+        }
+    }
+
+    impl Sealed for Placeholder {}
+}
