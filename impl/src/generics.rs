6b3e1e50b27d9f90fd4a4be098d4693e50609784
David Tolnay <dtolnay@gmail.com>
2025-01-08 11:41:16 -0800

    Generate trait bounds on associated types

--- impl/src/generics.rs
+++ impl/src/generics.rs
@@ -25,11 +25,12 @@ impl<'a> ParamsInScope<'a> {
 
 fn crawl(in_scope: &ParamsInScope, ty: &Type, found: &mut bool) {
     if let Type::Path(ty) = ty {
-        if ty.qself.is_none() {
-            if let Some(ident) = ty.path.get_ident() {
-                if in_scope.names.contains(ident) {
-                    *found = true;
-                }
+        if let Some(qself) = &ty.qself {
+            crawl(in_scope, &qself.ty, found);
+        } else {
+            let front = ty.path.segments.first().unwrap();
+            if front.arguments.is_none() && in_scope.names.contains(&front.ident) {
+                *found = true;
             }
         }
         for segment in &ty.path.segments {
