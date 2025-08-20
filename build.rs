cbc1ebad3e91621ee0f94cf56d131f12fee62a3c
David Tolnay <dtolnay@gmail.com>
2025-08-20 07:31:39 -0700

    Delete duplicate cap-lints flag from build script

--- build.rs
+++ build.rs
@@ -120,7 +120,6 @@ fn compile_probe(rustc_bootstrap: bool) -> bool {
         .arg("--crate-type=lib")
         .arg("--cap-lints=allow")
         .arg("--emit=dep-info,metadata")
-        .arg("--cap-lints=allow")
         .arg("--out-dir")
         .arg(&out_subdir)
         .arg(probefile);
