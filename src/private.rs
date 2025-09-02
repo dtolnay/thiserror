e4b163ea8df5fb9051d64a0f4d9f1d802636d1c1
Gaëtan Schwartz <>
2025-09-02 11:09:41 +0200

    feat: add From trait implementations for boxed types

--- src/private.rs
+++ src/private.rs
@@ -7,8 +7,14 @@ pub use crate::display::AsDisplay;
 pub use crate::provide::ThiserrorProvide;
 #[doc(hidden)]
 pub use crate::var::Var;
+#[cfg(not(feature = "std"))]
+#[doc(hidden)]
+pub use alloc::boxed::Box;
 #[doc(hidden)]
 pub use core::error::Error;
 #[cfg(all(feature = "std", not(thiserror_no_backtrace_type)))]
 #[doc(hidden)]
 pub use std::backtrace::Backtrace;
+#[cfg(feature = "std")]
+#[doc(hidden)]
+pub use std::boxed::Box;
